9c4504da67a3d2deaf49b100aeb0c60d8951bf89
Dong Zeng <zeng-dong@hotmail.com>
2023-01-02 13:24:56 -0600

    eventslist query and mapping profile

---
+++ _six/aspnet-core-6-clean-architecture/GloboTicket.TicketManagement/GloboTicket.TicketManagement.Application/Profiles/MappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using GloboTicket.TicketManagement.Application.Features.Events.Queries.GetEventsList;
+using GloboTicket.TicketManagement.Domain.Entities;
+
+namespace GloboTicket.TicketManagement.Application.Profiles;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Event, EventListVm>().ReverseMap();
+    }
+}
