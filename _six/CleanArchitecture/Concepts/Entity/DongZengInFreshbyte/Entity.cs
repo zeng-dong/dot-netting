a7edf8c9642bfac44f5391da64c7c860a5473ad2
Dong Zeng <zeng-dong@hotmail.com>
2023-01-06 13:57:52 -0600

    another

---
+++ _six/CleanArchitecture/Concepts/Entity/DongZengInFreshbyte/Entity.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concepts.Entity.DongZengInFreshbyte;
+
+public interface IEntity
+{
+    void AddEvent(IDomainEvent @event);
+
+    IEnumerable<IDomainEvent> GetEvents();
+
+    void ClearEvents();
+
+    Guid Id { get; set; }
+}
+
+public abstract class Entity : IEntity
+{
+    public Guid Id { get; set; }
+    protected virtual object Actual => this;
+    ICollection<IDomainEvent> Events { get; } = new List<IDomainEvent>();
+
+    public override bool Equals(object obj)
+    {
+        var other = obj as Entity;
+
+        if (other is null)
+            return false;
+
+        if (ReferenceEquals(this, other))
+            return true;
+
+        if (Actual.GetType() != other.Actual.GetType())
+            return false;
+
+        if (Id == Guid.Empty || other.Id == Guid.Empty)
+            return false;
+
+        return Id == other.Id;
+    }
+
+    public static bool operator ==(Entity a, Entity b)
+    {
+        if (a is null && b is null)
+            return true;
+
+        if (a is null || b is null)
+            return false;
+
+        return a.Equals(b);
+    }
+
+    public static bool operator !=(Entity a, Entity b)
+    {
+        return !(a == b);
+    }
+
+    public override int GetHashCode()
+    {
+        return Id.GetHashCode();
+    }
+
+    public object Clone()
+    {
+        return MemberwiseClone();
+    }
+
+    public void AddEvent(IDomainEvent @event)
+    {
+        Events.Add(@event);
+        EventAdded(@event);
+    }
+
+    protected virtual void EventAdded(IDomainEvent @event)
+    {
+    }
+
+    public IEnumerable<IDomainEvent> GetEvents()
+    {
+        return Events.OrderBy(_ => _.Timestamp);
+    }
+
+    public void ClearEvents()
+    {
+        Events.Clear();
+    }
+}
---
+++ _six/CleanArchitecture/Concepts/Entity/DongZengInFreshbyte/IDomainEvent.cs
@@ -0,0 +1,22 @@
+﻿namespace Concepts.Entity.DongZengInFreshbyte;
+
+public interface IDomainEvent
+{
+    public DateTime Timestamp { get; }
+}
+
+public record DomainEvent : IDomainEvent
+{
+    public DateTime Timestamp { get; } = Clock.Now;
+}
+
+public static class Clock
+{
+    static Func<DateTime> _nowGetter = () => DateTime.UtcNow;
+    public static DateTime Now => _nowGetter.Invoke();
+
+    public static void SetNowGetter(Func<DateTime> getter)
+    {
+        _nowGetter = getter;
+    }
+}
