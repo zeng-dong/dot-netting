dc3c06edee1e8aff6cbd90042d1f984c41117f0c
Dong Zeng <zeng-dong@hotmail.com>
2023-01-06 13:39:58 -0600

    another version

---
+++ _six/CleanArchitecture/Concepts/Entity/MilanJovanovic/Entity.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concepts.Entity.MilanJovanovic;
+
+public abstract class Entity : IEquatable<Entity>
+{
+    protected Entity(Guid id)
+        : this()
+    {
+        Ensure.NotEmpty(id, "The identifier is required.", nameof(id));
+
+        Id = id;
+    }
+
+    protected Entity()
+    {
+    }
+
+    public Guid Id { get; private set; }
+
+    public static bool operator ==(Entity a, Entity b)
+    {
+        if (a is null && b is null)
+        {
+            return true;
+        }
+
+        if (a is null || b is null)
+        {
+            return false;
+        }
+
+        return a.Equals(b);
+    }
+
+    public static bool operator !=(Entity a, Entity b) => !(a == b);
+
+    /// <inheritdoc />
+    public bool Equals(Entity other)
+    {
+        if (other is null)
+        {
+            return false;
+        }
+
+        return ReferenceEquals(this, other) || Id == other.Id;
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj is null)
+        {
+            return false;
+        }
+
+        if (ReferenceEquals(this, obj))
+        {
+            return true;
+        }
+
+        if (obj.GetType() != GetType())
+        {
+            return false;
+        }
+
+        if (!(obj is Entity other))
+        {
+            return false;
+        }
+
+        if (Id == Guid.Empty || other.Id == Guid.Empty)
+        {
+            return false;
+        }
+
+        return Id == other.Id;
+    }
+
+    public override int GetHashCode() => Id.GetHashCode() * 41;
+}
