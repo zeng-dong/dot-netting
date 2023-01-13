ad295ac1b8722fc650b590530efeb3d5fffcc809
Dong Zeng <zeng-dong@hotmail.com>
2023-01-13 13:29:18 -0600

    removed weather, added CachingTestController

---
+++ _six/Features/Caching/Distributed/Controllers/CachingTestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Distributed.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CachingTestController : ControllerBase
+{
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public CachingTestController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+}
