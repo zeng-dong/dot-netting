17e526246ff8622e268e558233dacd8a162c11d4
Dong Zeng <zeng-dong@hotmail.com>
2023-01-14 13:17:32 -0600

    tests for set and get T

--- _six/Features/Caching/DistributedTests/CacheServiceTests.cs
+++ _six/Features/Caching/DistributedTests/CacheServiceTests.cs
@@ -1,8 +1,11 @@
 using Distributed;
 using FluentAssertions;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
 using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json.Linq;
+using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using Xunit.Abstractions;
@@ -41,6 +44,26 @@ public class CacheServiceTests
         Then_the_instance_is_set_in_cache();
     }
 
+    [Fact]
+    public void Can_get_type_instance_when_exists()
+    {
+        Given_an_instance_exsits_in_cache();
+
+        When_retrieve_from_cache_as_type();
+
+        Then_the_instance_is_retrieved_from_cache();
+    }
+
+    [Fact]
+    public void Can_get_null_as_type_instalce_when_not_exist()
+    {
+        Given_an_instance_not_exsits_in_cache();
+
+        When_retrieve_from_cache_as_type();
+
+        Then_null_is_retrieved_from_cache();
+    }
+
     private void Given_byte_array()
     {
         _bytes = new byte[] { 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20 };
@@ -53,18 +76,22 @@ public class CacheServiceTests
 
     private void Given_an_instance()
     {
-        _instance = new()
-        {
-            Id = 123,
-            Name = "MD-5044",
-            Premium = 999,
-            Liability = new()
-            {
-                Id = 601,
-                Limit = 3500,
-                Deductible = 500
-            }
-        };
+        InitInstance();
+    }
+
+    private void Given_an_instance_exsits_in_cache()
+    {
+        InitInstance();
+
+        _innerImplementation
+            .SetAsync(_cacheKey,
+            Encoding.UTF8.GetBytes(JsonSerializer.Serialize(_instance, _jsonSerializerOptions)),
+            _distributedCacheEntryOptions);
+    }
+
+    private void Given_an_instance_not_exsits_in_cache()
+    {
+        _innerImplementation.Remove(_cacheKey);
     }
 
     private void When_set_to_cache_as_byte_array()
@@ -80,6 +107,11 @@ public class CacheServiceTests
     private void When_set_to_cache_as_type()
     {
         _cache.SetAsync(_cacheKey, _instance, _distributedCacheEntryOptions).Wait();
+    }
+
+    private void When_retrieve_from_cache_as_type()
+    {
+        _result = _cache.GetAsync<Policy>(_cacheKey).Result;
     }
 
     private void Then_the_byte_array_is_set_in_cache()
@@ -100,16 +132,36 @@ public class CacheServiceTests
         var entry = _innerImplementation.GetString(_cacheKey);
         entry.Should().NotBeNull();
 
-        var instance = JsonSerializer.Deserialize<Policy>(entry, new JsonSerializerOptions()
-        {
-            PropertyNamingPolicy = null,
-            WriteIndented = true,
-            AllowTrailingCommas = true,
-            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
-        });
+        var instance = JsonSerializer.Deserialize<Policy>(entry, _jsonSerializerOptions);
 
         _output.WriteLine($"{instance!.Name}: {instance.Premium}, {instance.Liability!.Limit}");
         instance.Should().BeEquivalentTo(_instance);
+    }
+
+    private void Then_the_instance_is_retrieved_from_cache()
+    {
+        _result.Should().BeEquivalentTo(_instance);
+    }
+
+    private void Then_null_is_retrieved_from_cache()
+    {
+        _result.Should().BeNull();
+    }
+
+    private void InitInstance()
+    {
+        _instance = new()
+        {
+            Id = 123,
+            Name = "MD-5044",
+            Premium = 999,
+            Liability = new()
+            {
+                Id = 601,
+                Limit = 3500,
+                Deductible = 500
+            }
+        };
     }
 
     public CacheServiceTests(ITestOutputHelper output)
@@ -125,10 +177,18 @@ public class CacheServiceTests
 
     private byte[]? _bytes;
     private string? _string;
-    private Policy? _instance;
+    private Policy? _instance, _result;
     private readonly IDistributedCache _innerImplementation;
     private readonly CachingService _cache;
     private readonly ITestOutputHelper _output;
     private const string _cacheKey = "policy-123";
     private readonly DistributedCacheEntryOptions _distributedCacheEntryOptions;
+
+    private static JsonSerializerOptions _jsonSerializerOptions = new()
+    {
+        PropertyNamingPolicy = null,
+        WriteIndented = true,
+        AllowTrailingCommas = true,
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+    };
 }
