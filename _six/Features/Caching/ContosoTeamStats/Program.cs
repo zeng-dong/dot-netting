832c70d2889de9d9fade7e01cdd460616c10b30f
Dong Zeng <zeng-dong@hotmail.com>
2023-01-18 17:59:50 -0600

    setup middleware; remove razor views

--- _six/Features/Caching/ContosoTeamStats/Program.cs
+++ _six/Features/Caching/ContosoTeamStats/Program.cs
@@ -5,18 +5,26 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+        var services = builder.Services;
 
         // Add services to the container.
-        builder.Services.AddRazorPages();
+        //builder.Services.AddRazorPages();
+
+        services.AddControllersWithViews();
+        services.AddSingleton(async x => await RedisConnection.InitializeAsync(connectionString: "localhost");
 
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
         if (!app.Environment.IsDevelopment())
         {
-            app.UseExceptionHandler("/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            //// app.UseExceptionHandler("/Error");
+            app.UseExceptionHandler("/Home/Error");
             app.UseHsts();
+        }
+        else
+        {
+            app.UseDeveloperExceptionPage();
         }
 
         app.UseHttpsRedirection();
@@ -26,7 +34,14 @@ public class Program
 
         app.UseAuthorization();
 
-        app.MapRazorPages();
+        ////app.MapRazorPages();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+        });
 
         app.Run();
     }
