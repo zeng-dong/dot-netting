0d338a3f935fb4b6da07201090ffae619688bdd9
Dong Zeng <zeng-dong@hotmail.com>
2023-02-01 19:06:07 -0600

    first working durable func

--- _six/Features/AzureFunctions/durable-functions-with-vs/Function1.cs
+++ _six/Features/AzureFunctions/durable-functions-with-vs/Function1.cs
@@ -1,11 +1,10 @@
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.DurableTask;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace durable_functions_with_vs
 {
