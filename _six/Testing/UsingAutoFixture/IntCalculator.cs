7adb348a21c4d08d35a899427e5811fdf5cd4e6c
Dong Zeng <zeng-dong@hotmail.com>
2023-02-03 20:14:48 -0600

    first good old test

---
+++ _six/Testing/UsingAutoFixture/IntCalculator.cs
@@ -0,0 +1,10 @@
+﻿namespace UsingAutoFixture;
+
+public class IntCalculator
+{
+    public int Value { get; private set; }
+
+    public void Subtract(int number) => Value -= number;
+
+    public void Add(int number) => Value += number;
+}
---
+++ _six/Testing/UsingAutoFixture/IntCalculatorTests.cs
@@ -0,0 +1,14 @@
+﻿namespace UsingAutoFixture;
+
+public class IntCalculatorTests
+{
+    [Fact]
+    public void Can_sub()
+    {
+        var calculator = new IntCalculator();
+
+        calculator.Subtract(1);
+
+        calculator.Value.Should().BeLessThan(0);
+    }
+}
