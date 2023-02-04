349a75f280d35cebfb3f86fe6df0a73c663843d2
Dong Zeng <zeng-dong@hotmail.com>
2023-02-03 20:22:18 -0600

    first autofixture test

--- _six/Testing/UsingAutoFixture/IntCalculatorTests.cs
+++ _six/Testing/UsingAutoFixture/IntCalculatorTests.cs
@@ -3,11 +3,26 @@
 public class IntCalculatorTests
 {
     [Fact]
+    [Trait("Category", "traditional")]
     public void Can_sub()
     {
         var calculator = new IntCalculator();
 
-        calculator.Subtract(1);
+        var toBeSubtracted = 1;
+        calculator.Subtract(toBeSubtracted);
+
+        calculator.Value.Should().BeLessThan(0);
+    }
+
+    [Fact]
+    [Trait("Category", "fixture")]
+    public void Can_sub_fixture_style()
+    {
+        var calculator = new IntCalculator();
+        var fixture = new Fixture();
+        var toBeSubtracted = fixture.Create<int>();
+
+        calculator.Subtract(toBeSubtracted);
 
         calculator.Value.Should().BeLessThan(0);
     }
