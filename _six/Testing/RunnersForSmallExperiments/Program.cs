720e2ca8fa0a2cd4c8a97375938db6bd9075e190
Dong Zeng <zeng-dong@hotmail.com>
2023-02-07 10:11:10 -0600

    cannot use c# 11 features in .net 6

---
+++ _six/Testing/RunnersForSmallExperiments/Program.cs
@@ -0,0 +1,4 @@
+﻿/// I cannot do this with .net 6, it says: CS8936 Feature "raw string literals" is not available in c# 10.0
+/// var rawString = """<html lang=""en"" />""";
+
+Console.WriteLine("Hello, World!");
