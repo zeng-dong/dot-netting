7df482a4fb8d0de476032b004a5f923a8c31ebe6
Dong Zeng <zeng-dong@hotmail.com>
2023-02-14 21:18:54 -0600

    in test I can create IOption and set it into another type which depends on it

---
+++ _six/Testing/UsingBuilders/SampleRepositoryTests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.Options;
+using TestingDomain.Options;
+
+namespace UsingBuilders;
+
+public class SampleRepositoryTests
+{
+    private readonly IOptions<SampleOptions> _options;
+    private readonly SampleRepository _repo;
+
+    public SampleRepositoryTests()
+    {
+        _options = Options.Create<SampleOptions>(new SampleOptions());
+
+        _repo = new(_options);
+    }
+
+    [Fact]
+    public void It_returns_true()
+    {
+        var result = _repo.Get().Result;
+        result.Should().BeTrue();
+    }
+}
