c70567af71aa925719ef4889fe26763b9990789b
Dong Zeng <zeng-dong@hotmail.com>
2023-02-17 09:46:11 -0600

    starting generics best practices proj

---
+++ _six/Features/Generics/BestPractices/Entities/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace BestPractices.Entities;
+
+public class Student
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    public Student(string firstName, string lastName)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+    }
+}
