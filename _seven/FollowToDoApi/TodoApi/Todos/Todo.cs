30727f836ad82291caaaf49baad4ce2cfde57779
Dong Zeng <zeng-dong@hotmail.com>
2023-02-19 20:00:11 -0600

    todo RouteGroupBuilder

---
+++ _seven/FollowToDoApi/TodoApi/Todos/Todo.cs
@@ -0,0 +1,39 @@
+﻿namespace TodoApi.Todos;
+
+using System.ComponentModel.DataAnnotations;
+
+public class Todo
+{
+    public int Id { get; set; }
+
+    [Required]
+    public string Title { get; set; } = default!;
+
+    public bool IsComplete { get; set; }
+
+    [Required]
+    public string OwnerId { get; set; } = default!;
+}
+
+public class TodoItem
+{
+    public int Id { get; set; }
+
+    [Required]
+    public string Title { get; set; } = default!;
+
+    public bool IsComplete { get; set; }
+}
+
+public static class TodoMappingExtensions
+{
+    public static TodoItem AsTodoItem(this Todo todo)
+    {
+        return new()
+        {
+            Id = todo.Id,
+            Title = todo.Title,
+            IsComplete = todo.IsComplete,
+        };
+    }
+}
---
+++ _seven/FollowToDoApi/TodoApi/Todos/TodoApi.cs
@@ -0,0 +1,11 @@
+﻿namespace TodoApi.Todos;
+
+internal static class TodoApi
+{
+    public static RouteGroupBuilder MapTodos(this IEndpointRouteBuilder routes)
+    {
+        var group = routes.MapGroup("/todos");
+
+        return group;
+    }
+}
