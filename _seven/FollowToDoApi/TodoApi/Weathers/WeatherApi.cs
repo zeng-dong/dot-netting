f1f6783e0fdb956f16fcaa1008d6291ab17d463d
Dong Zeng <zeng-dong@hotmail.com>
2023-02-19 20:13:47 -0600

    moving get weatherforcast to RouteGroupBuilder

---
+++ _seven/FollowToDoApi/TodoApi/Weathers/WeatherApi.cs
@@ -0,0 +1,28 @@
+﻿namespace TodoApi.Weathers;
+
+internal static class WeatherApi
+{
+    public static RouteGroupBuilder MapWeathers(this IEndpointRouteBuilder routes)
+    {
+        var group = routes.MapGroup("/weathers");
+
+        var summaries = new[] { "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" };
+
+        group.MapGet("/", () =>
+        {
+            var forecast = Enumerable.Range(1, 5).Select(index =>
+                new WeatherForecast
+                (
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    summaries[Random.Shared.Next(summaries.Length)]
+                ))
+                .ToArray();
+            return forecast;
+        })
+        .WithName("GetWeatherForecast")
+        .WithOpenApi();
+
+        return group;
+    }
+}
