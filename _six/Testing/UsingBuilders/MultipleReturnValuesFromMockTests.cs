14e9599f4780622695a69cf41ca430b7834ca2a5
Dong Zeng <zeng-dong@hotmail.com>
2023-02-21 11:45:53 -0600

    tried setupsequence

--- _six/Testing/UsingBuilders/MultipleReturnValuesFromMockTests.cs
+++ _six/Testing/UsingBuilders/MultipleReturnValuesFromMockTests.cs
@@ -1,11 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FluentAssertions;
+using Moq;
+using TestingDomain.NestedServices;
 
 namespace UsingBuilders;
 
 public class MultipleReturnValuesFromMockTests
 {
+    [Fact]
+    public void Mock_multiple_values()
+    {
+        Mock<ISupporting1> service1 = new Mock<ISupporting1>();
+        Mock<ISupporting2> service2 = new Mock<ISupporting2>();
+
+        service1
+            .SetupSequence(x => x.Do())
+            .Returns(true)
+            .Returns(false);
+
+        service2
+            .Setup(x => x.Do())
+            .Returns(true);
+
+        BlobService sut = new BlobService();
+        var callItFirstTime = sut.Do(service1.Object, service2.Object, "hello");
+        var callItSecondTime = sut.Do(service1.Object, service2.Object, "hello");
+        var callItThirdTime = sut.Do(service1.Object, service2.Object, "hello");
+
+        callItFirstTime.Should().Be(201);
+        callItSecondTime.Should().Be(401);
+        callItThirdTime.Should().Be(202);
+    }
 }
