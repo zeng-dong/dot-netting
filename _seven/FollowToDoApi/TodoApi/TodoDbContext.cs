d6199c40c291f8904d40745553dfe37d53b9fafb
Dong Zeng <zeng-dong@hotmail.com>
2023-02-20 21:31:21 -0600

    sqlite and todoContext

---
+++ _seven/FollowToDoApi/TodoApi/TodoDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApi.Todos;
+
+namespace TodoApi;
+
+public class TodoDbContext : DbContext
+{
+    public DbSet<Todo> Todos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=Todos.db");
+    }
+}
