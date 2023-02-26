392a39dfe2ad7547f2dd1cf113a0588cb0768cb5
Dong Zeng <zeng-dong@hotmail.com>
2023-02-26 12:12:38 -0600

    added exception handling middleware

---
+++ _six/Testing/TestingApi/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,45 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace TestingApi;
+
+public class ExceptionHandlingMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ExceptionHandlingMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception error)
+        {
+            var response = context.Response;
+            response.ContentType = "application/json";
+
+            switch (error)
+            {
+                case InvalidOperationException e:
+                    response.StatusCode = (int)HttpStatusCode.BadRequest;
+                    break;
+
+                case ArgumentException e:
+                    response.StatusCode = (int)HttpStatusCode.NotFound;
+                    break;
+
+                default:
+                    response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    break;
+            }
+
+            var result = JsonSerializer.Serialize(new { message = error?.Message });
+            await response.WriteAsync(result);
+        }
+    }
+}
