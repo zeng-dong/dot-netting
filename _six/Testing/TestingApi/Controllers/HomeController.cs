82a728855a42d8369eb425fb9cb74ab7c0151c57
Dong Zeng <zeng-dong@hotmail.com>
2023-02-26 11:59:21 -0600

    added testing controller

---
+++ _six/Testing/TestingApi/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestingApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+
+public class HomeController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Index([FromQuery] string name)
+    {
+        if (name == null) { throw new ArgumentNullException("name"); }
+
+        if (name == "null") { throw new ArgumentException("null cannot be hello"); }
+
+        return Ok($"Hello {name}");
+    }
+}
