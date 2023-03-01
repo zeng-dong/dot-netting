928b7a9ae6a2f7423098f52d3de4c61853f2ee52
Dong Zeng <zeng-dong@hotmail.com>
2023-03-01 17:44:54 -0600

    IEqualityComparer probably the simplest

---
+++ _six/Testing/TestingDomain/Objects/TermComparable.cs
@@ -0,0 +1,64 @@
+﻿namespace TestingDomain.Objects;
+
+public class TermComparable : IComparable<TermComparable>
+{
+    public int CompareTo(TermComparable? other)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(this, obj))
+        {
+            return true;
+        }
+
+        if (ReferenceEquals(obj, null))
+        {
+            return false;
+        }
+
+        throw new NotImplementedException();
+    }
+
+    public override int GetHashCode()
+    {
+        throw new NotImplementedException();
+    }
+
+    public static bool operator ==(TermComparable left, TermComparable right)
+    {
+        if (ReferenceEquals(left, null))
+        {
+            return ReferenceEquals(right, null);
+        }
+
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(TermComparable left, TermComparable right)
+    {
+        return !(left == right);
+    }
+
+    public static bool operator <(TermComparable left, TermComparable right)
+    {
+        return ReferenceEquals(left, null) ? !ReferenceEquals(right, null) : left.CompareTo(right) < 0;
+    }
+
+    public static bool operator <=(TermComparable left, TermComparable right)
+    {
+        return ReferenceEquals(left, null) || left.CompareTo(right) <= 0;
+    }
+
+    public static bool operator >(TermComparable left, TermComparable right)
+    {
+        return !ReferenceEquals(left, null) && left.CompareTo(right) > 0;
+    }
+
+    public static bool operator >=(TermComparable left, TermComparable right)
+    {
+        return ReferenceEquals(left, null) ? ReferenceEquals(right, null) : left.CompareTo(right) >= 0;
+    }
+}
---
+++ _six/Testing/TestingDomain/Objects/TermEqualityComparer.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace TestingDomain.Objects;
+
+public class TermEqualityComparer : IEqualityComparer<TermEqualityComparer>
+{
+    public bool Equals(TermEqualityComparer? x, TermEqualityComparer? y)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int GetHashCode([DisallowNull] TermEqualityComparer obj)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class TermEquatable : IEquatable<TermEquatable>
+{
+    public bool Equals(TermEquatable? other)
+    {
+        throw new NotImplementedException();
+    }
+}
