291ee365d8c6b073a5accb9cce60c2585bc2db5c
Dong Zeng <zeng-dong@hotmail.com>
2023-03-05 10:07:55 -0600

    added memory cache

--- _six/Features/HostedServices/WeatherService.Api/Program.cs
+++ _six/Features/HostedServices/WeatherService.Api/Program.cs
@@ -6,7 +6,7 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
+        builder.Services.AddMemoryCache();
 
         builder.Services.AddControllers();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -25,7 +25,6 @@ public class Program
         app.UseHttpsRedirection();
 
         app.UseAuthorization();
-
 
         app.MapControllers();
 
