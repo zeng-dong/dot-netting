0cd03646e3623baf0e3438175aa52543d9773667
Dong Zeng <zeng-dong@hotmail.com>
2023-03-06 18:34:53 -0600

    quartz console: obtains an instance of the scheduler, starts it, then shuts it down

---
+++ _six/Features/HostedServices/QuartzConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using Quartz.Impl;
+using Quartz;
+
+namespace QuartzConsole;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        Console.WriteLine("obtains an instance of the scheduler, starts it, then shuts it down");
+
+        StdSchedulerFactory factory = new StdSchedulerFactory();
+        IScheduler scheduler = await factory.GetScheduler();
+
+        // and start it off
+        await scheduler.Start();
+
+        // some sleep to show what's happening
+        await Task.Delay(TimeSpan.FromSeconds(10));
+
+        // and last shut down the scheduler when you are ready to close your program
+        await scheduler.Shutdown();
+    }
+}
