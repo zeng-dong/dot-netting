927ab5e2942e2d6241b379c6745fedf8ffc507af
Dong Zeng <zeng-dong@hotmail.com>
2023-03-06 18:04:42 -0600

    clean up

--- _six/Features/HostedServices/WeatherService.Api/Program.cs
+++ _six/Features/HostedServices/WeatherService.Api/Program.cs
@@ -9,13 +9,11 @@ public class Program
         builder.Services.AddMemoryCache();
 
         builder.Services.AddControllers();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
 
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
             app.UseSwagger();
