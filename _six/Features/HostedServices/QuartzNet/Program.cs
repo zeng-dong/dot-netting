f9ff23bb3e4591faa6c6ab55d8d54f6cdd7f737e
Dong Zeng <zeng-dong@hotmail.com>
2023-03-06 18:27:21 -0600

    config quartz with appsettings, register with extension method

--- _six/Features/HostedServices/QuartzNet/Program.cs
+++ _six/Features/HostedServices/QuartzNet/Program.cs
@@ -13,17 +13,19 @@ public class Program
             // use a scoped container to create jobs.
             q.UseMicrosoftDependencyInjectionScopedJobFactory();
 
-            // Create a "key" for the job
-            var jobKey = new JobKey("HelloWorldJob");
+            //// Create a "key" for the job
+            //var jobKey = new JobKey("HelloWorldJob");
 
-            // Register the job with the DI container
-            q.AddJob<HelloWorldJob>(opts => opts.WithIdentity(jobKey));
+            //// Register the job with the DI container
+            //q.AddJob<HelloWorldJob>(opts => opts.WithIdentity(jobKey));
 
-            // Create a trigger for the job
-            q.AddTrigger(opts => opts
-                .ForJob(jobKey) // link to the HelloWorldJob
-                .WithIdentity("HelloWorldJob-trigger") // give the trigger a unique name
-                .WithCronSchedule("0/15 * * * * ?")); // run every 15 seconds
+            //// Create a trigger for the job
+            //q.AddTrigger(opts => opts
+            //    .ForJob(jobKey) // link to the HelloWorldJob
+            //    .WithIdentity("HelloWorldJob-trigger") // give the trigger a unique name
+            //    .WithCronSchedule("0/15 * * * * ?")); // run every 15 seconds
+
+            q.AddJobAndTrigger<HelloWorldJob>(builder.Configuration);
         });
         builder.Services.AddQuartzHostedService(q => q.WaitForJobsToComplete = true);
 
