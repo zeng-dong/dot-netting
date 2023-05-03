34a2451001af1b3da8d9da77131cd275d2c54fe3
Dong Zeng <zeng-dong@hotmail.com>
2023-05-03 14:17:41 -0500

    starting the awarding bonus mini

---
+++ _six/Testing/TestingApi/AwardingBonus/IHumanResourceService.cs
@@ -0,0 +1,16 @@
+﻿using TestingDomain.Users;
+
+namespace TestingApi.AwardingBonus;
+
+public interface IHumanResourceService
+{
+    IList<Employee> ThoseDeservingBonus();
+}
+
+public class HumanResourceService : IHumanResourceService
+{
+    public IList<Employee> ThoseDeservingBonus()
+    {
+        throw new NotImplementedException();
+    }
+}
