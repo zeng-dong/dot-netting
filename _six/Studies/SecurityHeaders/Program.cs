50757671b629cbd9247b77665bd93f9a04a782f9
Dong Zeng <zeng-dong@hotmail.com>
2023-06-13 10:31:11 -0500

    trying andrelock securityheaders pkg

--- _six/Studies/SecurityHeaders/Program.cs
+++ _six/Studies/SecurityHeaders/Program.cs
@@ -1,11 +1,13 @@
+using Microsoft.Net.Http.Headers;
 using SecurityHeaders;
+using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.WebHost.ConfigureKestrel(serverOptions =>
-{
-    serverOptions.AddServerHeader = false;
-});
+//builder.WebHost.ConfigureKestrel(serverOptions =>
+//{
+//    serverOptions.AddServerHeader = false;
+//});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -21,9 +23,51 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+///app.UseSecurityHeaders();
+var policyCollection = new HeaderPolicyCollection()
+        .AddFrameOptionsDeny()
+        .AddXssProtectionBlock()
+        .AddContentTypeOptionsNoSniff()
+        .AddStrictTransportSecurityMaxAgeIncludeSubDomains(maxAgeInSeconds: 60 * 60 * 24 * 365) // maxage = one year in seconds
+        .AddReferrerPolicyStrictOriginWhenCrossOrigin()
+        .RemoveServerHeader()
+        .AddContentSecurityPolicy(builder =>
+        {
+            builder.AddObjectSrc().None();
+            builder.AddFormAction().Self();
+            builder.AddFrameAncestors().None();
+        })
+        .AddCrossOriginOpenerPolicy(builder =>
+        {
+            builder.SameOrigin();
+        })
+        .AddCrossOriginEmbedderPolicy(builder =>
+        {
+            builder.RequireCorp();
+        })
+        .AddCrossOriginResourcePolicy(builder =>
+        {
+            builder.SameOrigin();
+        })
+        .AddCustomHeader("X-My-Test-Header", "Header value");
+
+app.UseSecurityHeaders(policyCollection);
+
 app.UseHttpsRedirection();
 
 app.UseHttpMethodValidation();
+
+//app.Use(async (context, next) =>
+//{
+//    context.Response.Headers.Add("Referrer-Policy", "strict-origin-when-cross-origin");
+//    ////context.Response.Headers[HeaderNames.ContentType] = "nosniff";
+//    context.Response.Headers[HeaderNames.ContentSecurityPolicy] =
+//        "default-src 'none'; script-src 'self'; connect-src 'self'; img-src 'self'; style-src 'self'; frame-ancestors 'self'; form-action 'self';";
+//    ///context.Response.Headers[HeaderNames.XFrameOptions] = "x1";
+//    ///context.Response.Headers[HeaderNames.XContentTypeOptions] = "X2";
+
+//    await next();
+//});
 
 app.UseAuthorization();
 
