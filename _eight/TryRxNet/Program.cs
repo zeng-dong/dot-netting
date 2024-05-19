fecf8c6684c0f003848bc34cac338707dab1cb3c
Dong Zeng <zeng-dong@hotmail.com>
2024-05-19 10:35:28 -0500

    trying rxnet in net8

---
+++ _eight/TryRxNet/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Reactive.Linq;
+
+IObservable<long> ticks = Observable.Timer(
+    dueTime: TimeSpan.Zero,
+    period: TimeSpan.FromSeconds(1));
+
+ticks.Subscribe(tick => Console.WriteLine($"Tick {tick}"));
+
+Console.ReadLine();
