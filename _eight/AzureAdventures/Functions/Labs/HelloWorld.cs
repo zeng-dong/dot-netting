24c2500ff9001cb4de775a1f187a726d8459cd97
Dong Zeng <zeng-dong@hotmail.com>
2024-10-06 10:33:38 -0500

    reading req in func

--- _eight/AzureAdventures/Functions/Labs/HelloWorld.cs
+++ _eight/AzureAdventures/Functions/Labs/HelloWorld.cs
@@ -1,7 +1,10 @@
+using Azure.Core;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
+using System.Net;
 
 namespace Functions.Labs
 {
@@ -19,6 +22,20 @@ namespace Functions.Labs
         {
             _logger.LogInformation("HTTP triggered boo.");
             return new OkObjectResult("Hello world from boo");
+        }
+
+        [Function("ReadingFromRequest")]
+        public async Task<HttpResponseData> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "ReadingFromRequest/{name}")]
+            HttpRequestData req,
+            string name)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            _logger.LogInformation(name);
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+            await response.WriteStringAsync("Hello you entered: " + name);
+            return response;
         }
     }
 }
