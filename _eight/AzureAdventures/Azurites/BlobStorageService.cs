a5293d18dd7c9689fd278254afa6a9e6eaf22fe2
Dong Zeng <zeng-dong@hotmail.com>
2024-10-12 14:23:50 -0500

    upload image to blob storage

--- _eight/AzureAdventures/Azurites/BlobStorageService.cs
+++ _eight/AzureAdventures/Azurites/BlobStorageService.cs
@@ -1,17 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Azure.Storage.Blobs;
 
 namespace Azurites;
 
 internal class BlobStorageService : IStorageService
 {
-    public Task Execute()
+    public async Task Execute()
     {
         var connectionString = "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;";
 
-        return Task.CompletedTask;
+        var client = new BlobContainerClient(connectionString, "zd-images");
+        var stream = await new HttpClient().GetStreamAsync("https://hacknowledge.com/wp-content/uploads/2021/05/Unknown.png");
+
+        var blob = client.GetBlobClient("zd-blobStorage.png");
+        await blob.UploadAsync(stream);
+
+        Console.WriteLine($"Image {blob.Name} was uploaded successfully!");
     }
 }
--- _eight/AzureAdventures/Azurites/Program.cs
+++ _eight/AzureAdventures/Azurites/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Azurites;
+
+Console.WriteLine("Hello, You see Azurite enabling 3 types of storages!");
+
+List<IStorageService> services = [new BlobStorageService()];
+
+var tasks = services.Select(x => x.Execute());
+await Task.WhenAll(tasks);
+
+Console.WriteLine("Finished running services");
+Console.ReadKey();
