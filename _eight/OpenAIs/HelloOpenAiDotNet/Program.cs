089382b5eaf7c44ea90913faf605b3f09ca1ad88
Dong Zeng <zeng-dong@hotmail.com>
2024-10-14 18:09:31 -0500

    try open ai lib

---
+++ _eight/OpenAIs/HelloOpenAiDotNet/Program.cs
@@ -0,0 +1,9 @@
+﻿using OpenAI.Chat;
+
+ChatClient client = new ChatClient(
+    model: "gpt-4o",
+    apiKey: "");
+
+ChatCompletion completion = client.CompleteChat("what is the capital of China?");
+
+Console.WriteLine(completion.Content[0].Text);
