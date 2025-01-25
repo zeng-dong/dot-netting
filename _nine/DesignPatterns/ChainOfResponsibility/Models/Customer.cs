ec0d9baec3368d6358fae380c3c14bc6511457c1
Dong Zeng <zeng-dong@hotmail.com>
2025-01-24 19:55:38 -0600

    wip

---
+++ _nine/DesignPatterns/ChainOfResponsibility/Models/Customer.cs
@@ -0,0 +1,10 @@
+﻿namespace ChainOfResponsibility.Models;
+
+public class Customer
+{
+    public int Id { get; set; }
+    public string Surname { get; set; }
+    public string Forename { get; set; }
+    public decimal Discount { get; set; }
+    public string Address { get; set; }
+}
---
+++ _nine/DesignPatterns/ChainOfResponsibility/Models/Quote.cs
@@ -0,0 +1,9 @@
+﻿namespace ChainOfResponsibility.Models;
+
+public class Quote
+{
+    public int Id { get; set; }
+    public string Program { get; set; }
+    public string Product { get; set; }
+    public DateTime EffectiveDate { get; set; }
+}
