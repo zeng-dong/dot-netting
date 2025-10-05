9cc3ef821acd565d92c58f48c459c3d6eab53885
Dong Zeng <zeng-dong@hotmail.com>
2025-10-05 12:19:58 -0500

    copiloting first case

---
+++ _nine/Copiloting/Program.cs
@@ -0,0 +1,12 @@
+﻿using Copiloting.diagnosing;
+
+Console.WriteLine("Hello, Copiloting!");
+
+new Buggy().ExceptionIssue();
+
+/*
+ the application crashes when I remove item from goodies list. It then show the following stack dump: <copy stack trace>
+
+and Send to GitHub Copilot for analysis.
+
+ */
---
+++ _nine/Copiloting/diagnosing/Buggy.cs
@@ -0,0 +1,34 @@
+﻿namespace Copiloting.diagnosing;
+
+internal class Buggy
+{
+    public void ExceptionIssue()
+    {
+        Remove(2, ["a", "b", "c"]);
+    }
+
+    public void LogicIssue()
+    {
+        Remove(0, ["a", "b", "c"]);
+    }
+
+    public void Remove(int index, List<string> goodies)
+    {
+        if (index >= 0)
+        {
+            goodies.RemoveAt(index);
+
+            if (goodies.Count > 0)
+            {
+                var itemForLogging = goodies[index];
+
+                var logMessage = $"Removed item: {itemForLogging}";
+
+                Console.WriteLine($"Goodie removed: {logMessage}");
+            }
+        }
+
+        Console.WriteLine("Done removing goodies. The remaining: ");
+        goodies.ForEach(g => Console.WriteLine(g));
+    }
+}
