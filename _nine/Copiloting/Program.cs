db2a3764ad7e55fc761bf95fbe73d2af58176908
Dong Zeng <zeng-dong@hotmail.com>
2025-10-05 14:00:25 -0500

    ask gc to diagnose logical errors

--- _nine/Copiloting/Program.cs
+++ _nine/Copiloting/Program.cs
@@ -2,11 +2,14 @@
 
 Console.WriteLine("Hello, Copiloting!");
 
-new Buggy().ExceptionIssue();
+new Buggy().LogicIssue();
 
 /*
+ 1. exception:
  the application crashes when I remove item from goodies list. It then show the following stack dump: <copy stack trace>
 
 and Send to GitHub Copilot for analysis.
 
+    2. logical error:
+    The program says item b is removed but I expect item a is removed. Can you diagnose what has caused this and try to fix this?
  */
--- _nine/Copiloting/diagnosing/Buggy.cs
+++ _nine/Copiloting/diagnosing/Buggy.cs
@@ -31,4 +31,33 @@ internal class Buggy
         Console.WriteLine("Done removing goodies. The remaining: ");
         goodies.ForEach(g => Console.WriteLine(g));
     }
+
+    /*
+     In the shopping bascket an administration fee should be paid if the total price is below 500, but it also shows
+    no administration fee is paid while the total price is 489.91. Can you see how this can be caused from and fix this?
+     */
+
+    //public void RemoveByCopilot(int index, List<string> goodies)
+    //{
+    //    if (index >= 0 && index < goodies.Count)
+    //    {
+    //        var itemForLogging = goodies[index];
+    //        goodies.RemoveAt(index);
+
+    //        var logMessage = $"Removed item: {itemForLogging}";
+    //        Console.WriteLine($"Goodie removed: {logMessage}");
+
+    //        if (goodies.Count > 0)
+    //        {
+    //            Console.WriteLine("Remaining goodies:");
+    //            goodies.ForEach(g => Console.WriteLine(g));
+    //        }
+    //    }
+    //    else
+    //    {
+    //        Console.WriteLine("Index out of range. No item removed.");
+    //    }
+
+    //    Console.WriteLine("Done removing goodies.");
+    //}
 }
