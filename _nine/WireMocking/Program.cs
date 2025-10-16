9e0f06a5db2c4a3ded148812e48ab91edd9ab5f0
Dong Zeng <zeng-dong@hotmail.com>
2025-10-15 20:14:55 -0500

    start wiremock

---
+++ _nine/WireMocking/Program.cs
@@ -0,0 +1,13 @@
+﻿using WireMock.Server;
+using WireMock.Settings;
+
+Console.WriteLine("Hello, World!");
+
+var server = WireMockServer.Start(new WireMockServerSettings
+{
+    Urls = ["http://localhost:9091/"],
+    Port = 9091,
+    StartAdminInterface = true,
+    ReadStaticMappings = true,
+    WatchStaticMappings = true
+});
