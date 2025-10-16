3dce8bcf7e10ad0fddbdbfadfd9611e9893464d2
Dong Zeng <zeng-dong@hotmail.com>
2025-10-15 20:36:58 -0500

    first req res

--- _nine/WireMocking/Program.cs
+++ _nine/WireMocking/Program.cs
@@ -1,7 +1,10 @@
-﻿using WireMock.Server;
+﻿using System.Net.Mime;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+using WireMock.Server;
 using WireMock.Settings;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Wire Mocking Server Starting");
 
 var server = WireMockServer.Start(new WireMockServerSettings
 {
@@ -11,3 +14,17 @@ var server = WireMockServer.Start(new WireMockServerSettings
     ReadStaticMappings = true,
     WatchStaticMappings = true
 });
+
+server.Given(Request.Create().WithPath("/test").UsingGet())
+    .RespondWith(Response.Create()
+        .WithStatusCode(200)
+        .WithHeader("Content-Type", "application/json")
+        .WithBody("Welcome to WireMocking!"));
+
+
+Console.ReadLine();
+
+// create the sub
+/*
+1. Path /test
+ */
